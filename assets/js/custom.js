3cf4de18fdbd346de8bccc58bf24d59f79422159
rahmandika haekal <rahmandikahaekal31@gmail.com>
2017-01-14 07:38:43 +0700

    about content

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,6 +2,10 @@
 
 jQuery(document).ready(function() {
   
+  //lightbox
+  $(document).ready(function(){
+    $('a').nivoLightbox();
+  });
 	//this code for the croll animation
   $(document).ready(function () {
     $(document).on("scroll", onScroll);
@@ -48,16 +52,12 @@ function onScroll(event){
    jQuery(window).scroll(function() {
       var windowScrollPosTop = jQuery(window).scrollTop();
 
-      if(windowScrollPosTop >= 190) {
-        jQuery(".logo").css({"margin-top": "5px", "margin-bottom": "5px"});
-        jQuery(".navbar-default").css({"margin-top": "7px", "margin-bottom": "0"});
-        jQuery(".top-header").css({"background-color": "black"});
+      if(windowScrollPosTop >= 250) {
+        jQuery(".navbar-default").css({"margin-top": "0px", "margin-bottom": "0","background-color": "#000"});
         jQuery("#home .container .gallery .Logo").css({"margin": "170px auto 0px auto"});
       }
       else{
-        // jQuery(".logo").css({"margin-top": "8px", "border-bottom": "0"});
-        jQuery(".navbar-default").css({"margin-top": "25px", "margin-bottom": "15px"});
-        jQuery(".top-header").css({"background-color": "transparent"});
+        jQuery(".navbar-default").css({"margin-top": "25px", "margin-bottom": "15px","background-color": "transparent"});
         jQuery("#home .container .gallery .Logo").css({"margin": "170px auto 0px auto"});    
       }
    });
