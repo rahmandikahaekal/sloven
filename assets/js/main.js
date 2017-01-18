a083f7a760c0bbb05c4082e65e7911277cd694e0
rahmandika haekal <rahmandikahaekal31@gmail.com>
2017-01-18 07:43:32 +0700

    finishing work hover in content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
  new WOW().init();
+
+// Active isotope with jQuery code :
+$('.main_work').isotope({
+    itemSelector: '.item',
+    layoutMode: 'fitRows'
+});
+
+// Isotope click function
+$('.work_nav ul li').click(function() {
+    $('.work_nav ul li').removeClass('active');
+    $(this).addClass('active');
+    
+    var selector = $(this).attr('data-filter');
+    $('.main_work').isotope({
+        filter: selector
+    });
+    return false;
+})
